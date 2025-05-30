7948e8e933963de80028cb195662b7aaef1d4254
senf-f <mate.mrse@gmail.com>
2025-05-30 17:08:32 +0000

    optimiziran algoritam

--- src/grouping2.clj
+++ src/grouping2.clj
@@ -1,6 +1,6 @@
 (ns grouping2
   (:require [clojure.string :as str]
-           [clojure.set :as set]))
+            [clojure.set :as set]))
 
 (defn parse-names [input]
   (->> (str/split input #",")
@@ -8,9 +8,12 @@
        (remove empty?)))
 
 (defn assign-balanced-groups
-  "Distributes people with weights into n groups, balancing total group weights."
-  [people n-groups]
-  (let [sorted (sort-by (comp - second) people)] ; sort by weight descending (greedy assignment)
+  "Distributes people with weights into n groups, balancing total weights. Doubles shuffled, singles shuffled."
+  [singles doubles n-groups]
+  (let [shuffled-doubles (shuffle doubles)
+        shuffled-singles (shuffle singles)
+        people (concat (map #(vector % 2) shuffled-doubles)
+                       (map #(vector % 1) shuffled-singles))]
     (reduce (fn [groups [name weight]]
               (let [idx (->> groups
                              (map-indexed (fn [i g] [i (:weight g)]))
@@ -20,22 +23,21 @@
                                      {:members (conj (:members g) [name weight])
                                       :weight (+ (:weight g) weight)}))))
             (vec (repeat n-groups {:members [] :weight 0}))
-            sorted)))
+            people)))
 
 (defn -main []
   (println "Enter singles (comma-separated):")
   (let [singles (parse-names (read-line))]
     (println "Enter doubles (comma-separated):")
     (let [doubles (parse-names (read-line))
-          overlapping (clojure.set/intersection (set singles) (set doubles))]
+          overlapping (set/intersection (set singles) (set doubles))]
       (when (seq overlapping)
         (println "Warning: These names appear in both singles and doubles; treating them as doubles:" (str/join ", " overlapping)))
       (println "Enter the number of groups:")
       (let [n-groups (Integer/parseInt (str/trim (read-line)))
-            singles-list (map #(vector % 1) (remove (set overlapping) singles))
-            doubles-list (map #(vector % 2) doubles)
-            all-people (concat singles-list doubles-list)
-            groups (assign-balanced-groups all-people n-groups)]
+            singles-list (remove (set overlapping) singles)
+            doubles-list doubles
+            groups (assign-balanced-groups singles-list doubles-list n-groups)]
         (println "\nGroups (showing members and group weight):")
         (doseq [[idx group] (map-indexed vector groups)]
           (let [members-str (str/join ", " (map (fn [[name weight]]
@@ -45,6 +47,3 @@
                                                 (:members group)))]
             (println (format "Group %d (weight %d): %s"
                              (inc idx) (:weight group) members-str))))))))
-
-(when (= *file* (System/getProperty "babashka.file" *file*))
-  (-main))
